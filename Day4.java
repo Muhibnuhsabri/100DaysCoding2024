fee538af04d5ebcbae57296e574a12adc2d4d85b
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-05 17:22:07 +0800

    hari 4
    belajar buat print dan printf beseta perintah khusus

--- Day4.java
+++ Day4.java
@@ -4,10 +4,7 @@ public class Day4 {
 
     public static void main(String[] args) {
         
-        System.out.print("nama muhammad ibnuh sabri \n");
-        System.out.print("nim D0224503 \n");
-        System.out.print("kelas c \n");
-        
+        System.out.print("nama muhammad ibnuh sabri \nnim D0224503 \nkelas c \n");
         
         System.out.printf("Nama: %s%n", "muh ibnuh sabri");
         System.out.printf("Umur: %d tahun%n", 18);

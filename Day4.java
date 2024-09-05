aab969aca0c5efa3bba4a36177558d51a735ece5
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-05 09:59:12 +0800

    hari 4
    buat print dan belajar sedikit perintah khusus di printf

---
+++ Day4.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class Day4 {
+
+    public static void main(String[] args) {
+        
+        System.out.print("nama muhammad ibnuh sabri \n");
+        System.out.print("nim D0224503 \n");
+        System.out.print("kelas c \n");
+        
+        
+        System.out.printf("Nama: %s%n", "muh ibnuh sabri");
+        System.out.printf("Umur: %d tahun%n", 18);
+        System.out.printf("Tinggi Badan: %s cm%n", 170);
+        System.out.printf("Berat badan: %s kg%n", 50);
+        
+    }
+    
+}

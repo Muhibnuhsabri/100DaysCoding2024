4b00b2d0bd01f8c4ff2dab3f569a55fe9722d2d8
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-06 18:31:17 +0800

    hari 5
    membuat komentar

---
+++ day5.java
@@ -0,0 +1,14 @@
+package day5;
+public class Day5 {
+
+    public static void main(String[] args) {
+        
+        int angka = 10; // KOMENTAR UNTUK SATU BARIS
+        
+        /*
+        YANG INI DAPAT MEMASUKKAN 
+        LEBIH DARI 1 BARIS
+        */
+    }
+    
+}

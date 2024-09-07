3d25472d6858322d3c693e271e26d3b8fe39d748
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-07 14:23:36 +0800

    hari 6
    membuat variabel deklarasi

---
+++ Day6.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class Day6 {
+  
+    public static void main(String[] args) {
+        //deklarasi
+        String Nama = "muh ibnuh sabri";
+        String cm = "cm";
+        int Umur = 18;
+        double tinggiBadan = 170.2 ;
+        
+        //variabel
+        System.out.println("Nama: " + Nama);
+        System.out.println("Umur: " + Umur);
+        System.out.println("tinggi Badan: " + tinggiBadan + " cm");
+
+    }
+    
+}

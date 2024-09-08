86979487d54fff7bc0b95cc55afe9da94b86847c
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-08 11:25:35 +0800

    hari 7
    membuat inisialisasi

---
+++ day7.java
@@ -0,0 +1,13 @@
+package day7;
+
+public class Day07 {
+  
+    public static void main(String[] args) {
+        
+        String nama = "muh ibnuh sabri"; //Inisialisasi saat Deklarasi String
+        int umur = 18;                   //Inisialisasi saat Deklarasi Integral
+        double tinggi = 170.1;           //Inisialisasi saat Deklarasi Double 
+        char inisial = 'B';              //Inisialisasi saat Deklarasi karakter
+    }
+    
+}

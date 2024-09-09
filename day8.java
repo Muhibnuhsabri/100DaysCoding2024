7342cf549bc8b816f5eadd4c3de38190e23e3db5
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-09 16:19:42 +0800

    hari 8
    membuat update

---
+++ day8.java
@@ -0,0 +1,16 @@
+package day8;
+
+public class Day8 {
+
+    public static void main(String[] args) {
+        //deklarasi dan inisialisasi
+        int angka = 10;
+        System.out.println("nilai angka awal: " + angka);
+        
+        //update variabel
+        angka = 20;
+        System.out.println("nilai setelah update: " + angka);
+        
+    }
+    
+}

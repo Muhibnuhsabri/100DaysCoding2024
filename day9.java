33f81eb90f0d641bebb364f4933bea6ddd8f04af
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-10 18:47:37 +0800

    hari 9
    membuat constanta/final

---
+++ day9.java
@@ -0,0 +1,15 @@
+package day9;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+       final String Nama = "kominfo";
+       final int Umur = 18;
+       final double PI = 3.14159;
+       
+       System.out.println("Umur: " + Umur);
+       System.out.println("PI" + PI);
+       System.out.println("Nama:" + Nama);
+    }
+    
+}

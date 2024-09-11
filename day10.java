1bb5076f10a489ade2885b8644d6f91d69e8959d
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-11 16:44:25 +0800

    hari 10
    ukuran type data ( byte )

---
+++ day10.java
@@ -0,0 +1,21 @@
+package day10;
+
+public class Day10 {
+
+    public static void main(String[] args) {
+        
+        // Tipe Data Byte
+        
+        byte TipeDataByte = 127; 
+        
+        /*
+        ukuran type data byte itu ukuran 1 byte(8 bit)
+        yang paling kecil kapasitasnya, dibanding
+        type data yang lain dengan rentan -128 sampai 127
+        */
+        
+        System.out.println("Angka Tipe Data Byte :" +  TipeDataByte);
+        
+    }
+    
+}

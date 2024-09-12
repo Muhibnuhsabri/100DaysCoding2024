b7b96d341c9bc062dca1e07b0bc1afd5c7d1fd6c
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-12 21:33:31 +0800

    hari 11
    membuat type data short dan memasukkan nilai minimal dan maksimalnya

---
+++ day11.java
@@ -0,0 +1,14 @@
+package day11;
+
+public class Day11 {
+
+    public static void main(String[] args) {
+        /*
+        type data short di memiliki kapasitas lebih besar
+        dibandingkan type daya byte dengan rentan -32768 sampai
+        */
+        short minimal = -32768;
+        short maksimal = 32767;
+    }
+    
+}

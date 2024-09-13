9cc12d7ccfdf827141ff54d4ca8ff890245aef72
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-13 17:51:44 +0800

    hari 12
    membuat type data integer beserta penjelasannya

---
+++ day12/day12.java
@@ -0,0 +1,20 @@
+package day12;
+
+public class Day12 {
+
+    public static void main(String[] args) {
+        
+        /*
+        tybe data integer adalah 4Bit dan memiliki kapasitas yang lumayan besar
+        daripada type data byte maupun short tapi bukan
+        type data yang memiliki kapasitas paling besar
+        dengan rentan nilai minimal -2.147.483.648 hingga
+        2.147.483.647.
+        */
+
+        int berat_badan = 50;
+        int umur = 18;
+        
+    }
+    
+}

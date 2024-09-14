04c34d604a9dfb4f2486495761ccdef9dce5de7a
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-14 10:22:08 +0800

    evaluasi
    buatlah package dan class, dan di dalamnya buat 6 buah tipe data beserta nilainya, lalu lakukan println sebanyak 6x, dan 1 printf yang mencetak semua tipe data tersebut.

---
+++ Day13.java
@@ -0,0 +1,24 @@
+package Evaluasi;
+
+public class Evaluasi {
+
+    public static void main(String[] args) {
+        byte a = 11;
+        byte b = 12;
+        byte c = 14;
+        byte d = 15;
+        byte e = 16;
+        byte f = 17;
+        
+        System.out.println("angka 1: " + a);
+        System.out.println("angka 2: " + b);
+        System.out.println("angka 3: " + c);
+        System.out.println("angka 4: " + d);
+        System.out.println("angka 5: " + e);
+        System.out.println("angka 6: " + f);
+        
+        System.out.printf("angka1: %d angka2: %dangka3: %d angka4: %d angka 5: %d angka6: %d ",11,12,13,14,15,16);
+
+    }
+    
+}

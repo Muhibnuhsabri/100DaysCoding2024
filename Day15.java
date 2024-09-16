078a3e041d2016601a3152807fb1f8fb52320442
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-16 13:43:22 +0800

    hari 15
    membuat double dan beserta penjelasan nya

---
+++ Day15.java
@@ -0,0 +1,13 @@
+package day15;
+
+public class Day15 {
+
+    public static void main(String[] args) {
+        double UangSaya = 10.5;
+        double SisaUang = 5.5;
+          
+        System.out.println("uang yang saya bawa ke kampus: " +UangSaya );
+        System.out.println("sisa uang saya setelah pulang ke kampus: " +SisaUang);
+    }
+    
+}

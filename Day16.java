0e306c62b9cc9e9d42f513ad260cf0c9a240e19f
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-17 13:02:10 +0800

    hari 16
    membuat type data float beserta penjelasan nya

---
+++ Day16.java
@@ -0,0 +1,10 @@
+package day16;
+
+public class Day16 {
+
+    public static void main(String[] args) {
+        float gaji = 50.0f;
+        System.out.println("gaji saya jadi penjaga sandal: " + gaji);
+        
+    }
+}

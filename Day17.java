bb9b49e1106cbf2fd957d57cf652fb6d1f543dad
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-18 17:54:49 +0800

    hari 17
    membuat type data char

---
+++ Day17.java
@@ -0,0 +1,13 @@
+package day17;
+
+public class Day17 {
+   
+    public static void main(String[] args) {
+        
+    char jeniskelamin = 'L';
+    
+    System.out.println("jenis kelamin saya adalah: " + jeniskelamin);
+    
+    }
+    
+}

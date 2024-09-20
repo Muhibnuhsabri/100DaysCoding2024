0340aebb7f2bffa6b66d34ea0fc05bd2a9561988
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-20 17:37:22 +0800

    hari 19
    membuat referens string

---
+++ Day19.java
@@ -0,0 +1,15 @@
+package day19;
+
+public class Day19 {
+
+    public static void main(String[] args) {
+       
+        // String adalah tipe data referens di Java yang digunakan untuk menyimpan urutan karakter, termasuk huruf, angka, dan simbol
+        
+        String nama = "muh ibnu sabri";
+        
+        System.out.println("nama saya adalah: " + nama);
+        
+    }
+    
+}

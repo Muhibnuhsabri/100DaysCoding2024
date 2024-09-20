362ac6b56266011933cd23e04a33ca05a9578aa0
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-20 10:53:29 +0800

    hari 18
    membuat type data boolean

--- Day18.java
+++ Day18.java
@@ -4,10 +4,10 @@ public class Day18 {
 
     public static void main(String[] args) {
        
-        boolean mengerjakanHari19 = true;
-        boolean tidakMengerjakanHari19 = false;
+        boolean mengerjakanHari18 = true;
+        boolean tidakMengerjakanHari18 = false;
     
-    System.out.println("apakah ibnu sudah commit hari ini?: " + mengerjakanHari19);
+    System.out.println("apakah ibnu sudah commit hari ini?: " + mengerjakanHari18);
         
     }
     

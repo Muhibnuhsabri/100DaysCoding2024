e14578b07d1fc67b145185acda33e5f4dc745e55
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-21 23:23:43 +0800

    Hari 20
    Membuat type data referens byte

---
+++ Day20.java
@@ -0,0 +1,11 @@
+package day20;
+
+public class Day20 {
+
+    public static void main(String[] args) {
+
+        Byte hasil_si_A = 100; // Menggunakan tipe data Byte
+
+        System.out.println("Siswa A memiliki nilai tertinggi di sekolah dengan nilai: " + hasil_si_A);
+    }
+}

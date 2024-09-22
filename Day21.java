86e6f83524da8385a37913f2006d87afd778e3fe
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-23 00:12:03 +0800

    Hari 21
    Membuat type data referens short

--- Day21.java
+++ Day21.java
@@ -4,8 +4,8 @@ public class Day21 {
 
     public static void main(String[] args) {
 
-        short ContohUAS= 80;
-        short UjianAkhirSemester = 70;
+        short ContohUAS = 80; 
+        short UjianAkhirSemester = ContohUAS;
 
         System.out.println("Apakah ibnuh lulus? = " + UjianAkhirSemester);
 

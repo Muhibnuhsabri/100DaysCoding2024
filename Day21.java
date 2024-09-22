e491180d01a44ab5168bf966a27a76a54f94ecdf
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-22 23:07:24 +0800

    Hari 21
    Membuat type data referens short

---
+++ Day21.java
@@ -0,0 +1,20 @@
+package day21;
+
+public class day21 {
+
+    public static void main(String[] args) {
+
+        /*
+        variabel data short(type daya ini digunakan untuk menyimpan nilai bilangan bulat 
+        yang lebih besar dari pada type data byte.)
+        */
+
+        short nilai1 = 2340;
+        short nilai2 = 3450;
+        short niali3 = 450;
+
+        System.out.println("hasil = " + nilai1*nilai2/nilai3);
+
+    }
+
+}

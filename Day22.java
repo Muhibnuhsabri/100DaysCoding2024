4c7f8e8752c4f07f3aaa0b03177af893422971be
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-23 17:07:44 +0800

    hari 22
    membuat type data referens int

---
+++ Day22.java
@@ -0,0 +1,13 @@
+package day22;
+
+public class Day22 {
+
+    public static void main(String[] args) {
+        
+        Integer nilai = 2000;
+        
+        System.out.println("nilai: " + nilai);
+        
+    }
+    
+}

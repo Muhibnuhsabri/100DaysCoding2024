f78f260a269524dafa599496e71c0028097781f4
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-24 17:49:03 +0800

    hari 23
    membuat type data referens Long

---
+++ Day23.java
@@ -0,0 +1,13 @@
+package coding100day;
+
+public class Day_23 {
+    
+    public static void main(String[] args) {
+        
+        Long PI = 3_141_59L;
+        
+        System.out.println("nilai pi: " + PI);
+        
+    }
+    
+}

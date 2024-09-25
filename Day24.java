d6f747c462860b1c7f85522cec9809bdd4089ec9
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-25 14:29:39 +0800

    Hari 24
    Membuat type data referens Float

---
+++ Day24.java
@@ -0,0 +1,13 @@
+package coding100day;
+
+public class Day_24 {
+
+    public static void main(String[] args) {
+
+         Float berat_Badan = 50.0f;
+
+         System.out.println("berat badan saya: " + berat_badan);
+
+    }
+
+}

7362bc3d870d10aee92ee793a8b6ca2589398586
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-26 22:10:22 +0800

    Hari 25
    Membuat type data referens Double

---
+++ Day25.java
@@ -0,0 +1,13 @@
+package coding100day;
+
+public class Day_25 {
+
+    public static void main(String[] args) {
+
+         Double berat = 50.0;
+
+         System.out.println("berat badan saya adalah: "+berat);
+      
+   }
+  
+}

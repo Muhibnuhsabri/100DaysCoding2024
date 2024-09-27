7370845a60a1bd5b3969588d776e6799b348ad3e
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-27 21:08:54 +0800

    hari 26
    Membuat type data referens Character

---
+++ Day26.java
@@ -0,0 +1,12 @@
+package coding100day;
+
+public class Day_26 {
+    
+    public static void main(String[] args) {
+        
+        Character jenis_Kelamin = 'L';
+        
+        System.out.println("jenis kelamin saya adalah:" + jenis_Kelamin);
+    }
+    
+}

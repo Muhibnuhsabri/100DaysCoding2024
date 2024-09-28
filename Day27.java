48c7b5030ff94d35bc40ca31a2a0ec91262a8368
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-28 22:37:54 +0800

    hari 27
    evaluasi besar-besaran

---
+++ Day27.java
@@ -0,0 +1,20 @@
+package coding100day;
+
+public class evaluasiBesar {
+    
+    public static void main(String[] args) {
+        
+        //soal nomor 1
+        
+        System.out.println("selamat datang di perpustakan film favorit!");
+        final int jumlahFilm = 50;
+        String namaFilm = "boboyboy";
+        short tahunRilis = 2015;
+        namaFilm = "from";
+        
+        System.out.printf("film berjudul %s di rilis pada tahun %d dan terdapat %d film dalam katalog",namaFilm,tahunRilis,jumlahFilm);
+        
+        
+    }
+    
+}

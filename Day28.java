081100906a8b05d6ad0895d4b2e665dd297e3d3b
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-29 18:04:56 +0800

    Hari 28
    membuat type data referens Boolean

---
+++ Day28.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+public class Day_28 {
+    
+    public static void main(String[] args) {
+        
+        Boolean isTrue = true;
+        Boolean isFalse = false;
+        
+        System.out.println("nilai isTrue: " + isTrue);
+        System.out.println("nilai isFalse: " + isFalse);
+        
+    }
+    
+}

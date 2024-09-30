7b718c142f62560fa2bcfc02cc5fe077d5aba455
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-09-30 20:34:55 +0800

    hari 29
    membuat type data referens BigInteger

---
+++ Day29.java
@@ -0,0 +1,15 @@
+package coding100day;
+import java.math.BigInteger;
+
+public class Day_29 {
+    
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger ("2728377393");
+        BigInteger b = new BigInteger ("2747326486");
+        
+        System.out.println("hasil a = " +a);
+        System.out.println("hasil b = " +b);
+        
+    }
+    
+}

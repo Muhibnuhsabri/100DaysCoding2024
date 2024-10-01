f5dc56e5aa5cc2c15d8888a59bc448c3e72f79d1
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-01 10:02:14 +0800

    hari 30
    membuat type data referens BigDesimal

---
+++ Day30.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+import java.math.BigDecimal;
+
+public class Day_30 {
+    
+    public static void main(String[] args) {
+        
+        BigDecimal angkaBulat = new BigDecimal ("2223.0");
+        
+        System.out.println("angka desimalnya adalah "+angkaBulat);
+        
+    }
+    
+}

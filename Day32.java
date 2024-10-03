7ed295137ebb7f31acf4d2becd9ad4b07b3f0378
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-03 21:30:04 +0800

    hari 32
    membuat konversi type data ke string

---
+++ Day32.java
@@ -0,0 +1,18 @@
+package coding100day;
+
+public class Day_32 {
+    
+    public static void main(String[] args) {
+        
+        int buayaInt = 3;
+        double hargaDouble = 1.23;
+        
+        String jumlahBuaya = "jumlah buaya disini "   +String.valueOf(buayaInt);
+        String hargaBuaya = "harga buaya ini adalah ini hasilnya " +hargaDouble;
+        
+        System.out.println(jumlahBuaya);
+        System.out.println(hargaBuaya);
+        
+    }
+    
+}

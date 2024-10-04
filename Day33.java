5fa166ce3f54ebdc1fa9fe48f934fb98d23ce6c8
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-04 20:54:27 +0800

    hari 33
    membuat type casting

---
+++ Day33.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+public class Day_33 {
+    
+    public static void main(String[] args) {
+        //
+        double telur = 9.78;
+        int intIni = (int) telur;
+        
+        System.out.println(telur);
+        System.out.println(intIni);
+        
+    }
+    
+}

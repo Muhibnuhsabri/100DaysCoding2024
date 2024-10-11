27050bf5cf0fcb1c8d02b444a2cb20c82d17745c
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-11 12:22:45 +0800

    hari 40
    Membuat Operator Perbandingan : Lebih besar dan lebih kecil

---
+++ Day40.java
@@ -0,0 +1,20 @@
+package coding100day;
+import java.util.Scanner;
+public class Day_40 {
+    
+    public static void main(String[] args) {
+        
+        Scanner banding = new Scanner(System.in);
+        
+        System.out.println("masukkan nilai untuk huruf A");
+        int nilaiA = banding.nextInt();
+        
+        System.out.println("masukkan nilai untuk huruf B");
+        int nilaiB = banding.nextInt();
+        
+        System.out.println("apakah rill nilai A lebih besar di bandingkan nilai B? = "+(nilaiA > nilaiB));
+        System.out.println("apakah rill nilai A lebih kecil di bandingkan nilai B? = "+(nilaiA > nilaiB));
+        
+    }
+    
+}

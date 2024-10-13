3e184ba55089e2fe1245fac131910839163c9cdf
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-13 21:35:35 +0800

    hari 42
    Operator Perbandingan : Sama dengan dan tidak sama dengan

---
+++ Day42.java
@@ -0,0 +1,20 @@
+package coding100day;
+import java.util.Scanner;
+public class Day_41 {
+    
+    public static void main(String[] args) {
+        
+        Scanner cabai = new Scanner(System.in);
+        
+        System.out.println("harga awal cabai adalah ");
+        int hargaAwal = cabai.nextInt();
+        
+        System.out.println("harga cabai sekarang adalah ");
+        int hargaSekarang = cabai.nextInt();
+        
+        System.out.println("apakah cabai harga nya tetap? = "+(hargaAwal == hargaSekarang));
+        System.out.println("apakah cabai harga nya sudah berbeda? = "+(hargaAwal != hargaSekarang));
+        
+    }
+    
+}

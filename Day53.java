337bc9253ff855f7a91b7bfa108a9e1222eccf8c
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-24 22:18:17 +0800

    hari 53
    Looping : for

---
+++ Day53.java
@@ -0,0 +1,16 @@
+package coding100day;
+
+public class Day_53 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("angka dibawah ini adalah genap ");
+        for(int angka=2 ; angka<=6; angka+=2){
+            System.out.println(angka);
+        }
+        System.out.println("angka dibawah ini adalah ganjil");
+        for(int angka=1 ; angka<=20; angka+=2){
+            System.out.println(angka);
+        }
+    }
+}

87075e2d853cb811089e6025668326f7642de8cd
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-24 23:29:03 +0800

    Hari 53
    Looping : for

--- Day53.java
+++ Day53.java
@@ -4,13 +4,8 @@ public class Day_53 {
 
     public static void main(String[] args) {
         
-        System.out.println("angka dibawah ini adalah genap ");
-        for(int angka=2 ; angka<=6; angka+=2){
-            System.out.println(angka);
-        }
-        System.out.println("angka dibawah ini adalah ganjil");
-        for(int angka=1 ; angka<=20; angka+=2){
-            System.out.println(angka);
+        for(int angka=1 ; angka<=10;){
+            System.out.println("ayo ayo ganyang fufufafa, ganyang fufufafa Sekarang juta ");
         }
     }
 }

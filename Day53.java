3c8583903526a002abd05d4c8fb665727133eaee
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-24 23:30:17 +0800

    Hari 53
    Looping : for

--- Day53.java
+++ Day53.java
@@ -5,7 +5,7 @@ public class Day_53 {
     public static void main(String[] args) {
         
         for(int angka=1 ; angka<=10;){
-            System.out.println("ayo ayo ganyang fufufafa, ganyang fufufafa Sekarang juta ");
+            System.out.println("ayo ayo ganyang fufufafa, ganyang fufufafa Sekarang juga ");
         }
     }
 }

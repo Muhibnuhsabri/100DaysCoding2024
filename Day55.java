64c0f362292044bdfc8fad9d63cc09222037161b
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-26 18:56:54 +0800

    hari 55
    Looping : for Decrement

---
+++ Day55.java
@@ -0,0 +1,14 @@
+package coding100day;
+
+public class Day_55 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("angka 10 sampai angka 1 ");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}

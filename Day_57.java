421e179604f0767693ef04ed33d999a453961e33
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-28 20:49:39 +0800

    hari 57
    Looping : while Increment

---
+++ Day_57.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+public class Day_57 {
+    
+    public static void main(String[] args) {
+        
+        int a = 0;
+        
+        while (a <= 5) {            
+            System.out.println("perulangan ke - " +a);
+            a++;
+        }
+    }
+    
+}

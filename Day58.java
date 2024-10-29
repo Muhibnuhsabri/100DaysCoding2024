e043aa37d49469a37183417633f480b69ebc619d
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-29 22:46:05 +0800

    hari 58
    Looping : while Decrement

---
+++ Day58.java
@@ -0,0 +1,16 @@
+package coding100day;
+
+public class Day_58 {
+    
+    public static void main(String[] args) {
+        
+        
+        int a = 5;
+        
+        while (a >= 0) {            
+            System.out.println("perulangan ke - " +a);
+            a--;
+        }
+    }
+    
+}

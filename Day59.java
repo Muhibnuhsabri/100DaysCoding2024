fe7d4a12a5c75b084ab08c24bd7c7afeaa4cb55a
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-30 22:45:05 +0800

    hari 59
    Looping : For Each

---
+++ Day59.java
@@ -0,0 +1,12 @@
+package coding100day;
+
+public class Day_59 {
+    public static void main(String[] args) {
+        
+        String[] alat = {"laptop","mouse","cas","tas","buku"};
+        
+        for (String benda : alat ) {
+            System.out.println(benda+ "");
+        }   
+    }
+}

d2493050ea338814707817de85b4e54ac528ede5
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-10-31 21:06:50 +0800

    hari 60
    Looping : do while

---
+++ Day60.java
@@ -0,0 +1,16 @@
+package coding100day;
+
+public class Day_60 {
+    
+    public static void main(String[] args) {
+        
+        int a = 1;
+        
+        do {
+            System.out.println(a);
+            a++;
+        } while (a <= 5);
+        
+    }
+    
+}

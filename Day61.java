07399f2728c781b2bc8f99433f448ebc70f3ad22
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-02 17:58:57 +0800

    hari 61
    Infinite Loop

---
+++ Day61.java
@@ -0,0 +1,13 @@
+package coding100day;
+
+public class Day_61 {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; ; i++) {
+            System.out.println("a");
+        }
+
+    }
+    
+}

401f2bfda121a607fd7178059a34bb0c5691b886
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-02 17:57:48 +0800

    hari 62
    Keyword : break

---
+++ Day62.java
@@ -0,0 +1,16 @@
+package coding100day;
+
+public class Day_62 {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println("i = "+i);
+        }
+        
+    }
+    
+}

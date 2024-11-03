d82b8e2c8abb157494470b67ab30e88ae6dddb4a
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-03 21:37:30 +0800

    hari 63
    Keyword : continue

---
+++ Day63.java
@@ -0,0 +1,17 @@
+package coding100day;
+
+public class Day_63 {
+    
+    public static void main(String[] args) {
+        
+        
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println("bilangan Ganjil = " + i);
+            }
+        
+    }
+    
+}

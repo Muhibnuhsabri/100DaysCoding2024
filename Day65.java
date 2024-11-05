e194ac3527ddba143071419ee01f53bd884e9dad
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-05 15:40:57 +0800

    hari 65
    Nested Loop

---
+++ Day65.java
@@ -0,0 +1,18 @@
+package coding100day;
+
+public class Day_65 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("perkalian 1 sampai 10 ");
+        
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i +" kali "+j+" = " + i*j + "\t");
+                
+            }System.out.println("");
+        }
+        
+    }
+    
+}

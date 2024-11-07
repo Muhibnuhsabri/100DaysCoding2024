e0906137723a0eed0171889d77b45138cde2a8f5
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-07 22:23:36 +0800

    hari 67
    Method Void

---
+++ Day67.java
@@ -0,0 +1,18 @@
+package coding100day;
+
+public class Day_67 {
+    
+    public static void indonesia() {
+        
+        String a = "pak Prabowo";
+        System.out.println("untuk "+a+", buatlah indonesia menjadi lebih baik ");
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        indonesia();
+        
+    }
+    
+}

b27d7715a6e0a6a4e82d589ea9b5b89a259ba985
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-08 14:46:30 +0800

    evaluasi mentor 2
    evaluasi mentor 2

---
+++ Day41.java
@@ -0,0 +1,13 @@
+package coding100day;
+
+public class Day_41 {
+    
+    public static void main(String[] args) {
+        
+        //evaluasi mentor 2
+        
+        
+        
+    }
+    
+}

855bf44e5e230c9abd4851268e62111440dbd513
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-08 14:28:29 +0800

    hari 7
    Variabel : Inisialisasi

--- Day7.java
+++ Day7.java
@@ -8,6 +8,7 @@ public class Day07 {
         int umur = 18;                   //Inisialisasi saat Deklarasi Integral
         double tinggi = 170.1;           //Inisialisasi saat Deklarasi Double 
         char inisial = 'B';              //Inisialisasi saat Deklarasi karakter
+      
     }
     
 }

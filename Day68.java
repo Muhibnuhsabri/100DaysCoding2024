92a0dc857be4af42df54d4fd5a5110833c17e76f
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-08 22:53:59 +0800

    hari 68
    Method Non-void

---
+++ Day68.java
@@ -0,0 +1,20 @@
+package coding100day;
+
+public class Day_68 {
+    
+      public int AdanB (int a, int b) {
+        int tambah = a + b;
+        return tambah;
+          
+}
+    
+    public static void main(String[] args) {
+        
+        System.out.println("5 + 3 ");
+        Day_68 hasil = new Day_68();
+        int hasilTambah = hasil.AdanB(5,3);
+        System.out.println("hasil tambah = "+ hasilTambah);
+        
+    }
+    
+}

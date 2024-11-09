eb59e940f4b1d822526a0e9a864b96826c0ec006
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-09 22:39:01 +0800

    hari 69
    Method Parameter

---
+++ Day69.java
@@ -0,0 +1,22 @@
+package coding100day;
+
+public class Day_69 {
+    
+    public static int bukudanpulpen(int buku, int pulpen) {
+        
+        return buku+pulpen;
+                
+    }
+    
+    public static void main(String[] args) {
+        
+        int buku = 5;
+        int pulpen = 2;
+        
+        int hasil = bukudanpulpen (buku,pulpen);
+        
+        System.out.println("buku dan pulpen saya ada = "+hasil);
+        
+    }
+    
+}

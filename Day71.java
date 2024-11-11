4dfb00c8830a64edeee8bcb82346e8e3e3dba446
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-11 23:33:31 +0800

    hari 71
    Method Static

---
+++ Day71.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+public class Day_71 {
+    
+    public static String kalimat(String kata1, String kata2) {
+        return kata1 + " " + kata2;
+    }
+
+    public static void main(String[] args) {
+        
+        String hasil = Day_71.kalimat("Belajar", "Java");
+        System.out.println(hasil);
+        
+    }
+}

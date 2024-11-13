f5698014b44f273762297953204e3753c3b16905
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-13 21:29:16 +0800

    hari 73
    String Method : charAt()

---
+++ Day73.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+public class Day_73 {
+    
+    public static void main(String[] args) {
+        
+        String nama = "Muh ibnuh sabri";
+        
+        char hurufAwal = nama.charAt(0);
+        
+        System.out.println(hurufAwal);
+        
+    }
+    
+}

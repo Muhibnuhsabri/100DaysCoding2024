bd56ab928bed6acff7c404390909130a7f49b4c3
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-14 22:52:37 +0800

    hari 74
    String Method : length()

---
+++ Day74.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+public class Day_74 {
+    
+    public static void main(String[] args) {
+        
+        String kalimat = "otw akhir bulan!";
+        int panjangKalimat = kalimat.length();
+        
+        System.out.println("kalimat : "+kalimat);
+        System.out.println("panjang kalimat : "+panjangKalimat);
+        
+    }
+    
+}

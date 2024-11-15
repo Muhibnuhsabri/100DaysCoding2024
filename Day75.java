30c1c801500012caabe39d794b8946c7f525c66c
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-15 13:39:18 +0800

    hari 75
    String Method : toLowerCase()

---
+++ Day75.java
@@ -0,0 +1,13 @@
+package coding100day;
+
+public class Day_75 {
+    
+    public static void main(String[] args) {
+        
+        String kata = "SisTeM oUt pRinTLn";
+        String hurufKecil = kata.toLowerCase();
+        System.out.println(hurufKecil);
+        
+    }
+    
+}

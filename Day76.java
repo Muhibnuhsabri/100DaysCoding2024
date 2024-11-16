12d4b4f05b263b0050760cdc385ce3b7c6e9d310
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-16 16:11:18 +0800

    hari 76
    String Method : toUpperCase()

---
+++ Day76.java
@@ -0,0 +1,12 @@
+package coding100day;
+
+public class Day_76 {
+    
+    public static void main(String[] args) {
+        
+        String kata = "SisTeM oUt pRinTLn";
+        String hurufBesar = kata.toUpperCase();
+        System.out.println(hurufBesar);
+        
+    } 
+}

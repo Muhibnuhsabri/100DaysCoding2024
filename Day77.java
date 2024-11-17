77a2b92049001cb72a7d455d46bd34ca57e26076
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-17 18:31:56 +0800

    hari 77
    String Method : equals()

---
+++ Day77.java
@@ -0,0 +1,17 @@
+package coding100day;
+
+public class Day_77 {
+    
+    public static void main(String[] args) {
+        
+        String kata = "cat";
+        
+        String kata2 = "cat";
+        String kata3 = "car";
+        
+        System.out.println(kata.equals(kata2));
+        System.out.println(kata.equals(kata3));
+        
+    }
+    
+}

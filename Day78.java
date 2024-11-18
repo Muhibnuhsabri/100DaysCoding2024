311ecc34f23374f7119d35d5c1f768d1b1b1965b
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-18 20:22:26 +0800

    hari 78
    String Method : equalsIgnoreCase()

---
+++ Day78.java
@@ -0,0 +1,17 @@
+package coding100day;
+
+public class Day_78 {
+    
+    public static void main(String[] args) {
+        
+        String kata = "cat";
+        
+        String kata2 = "Cat"; 
+        String kata3 = "car";
+        
+        System.out.println(kata.equalsIgnoreCase(kata2)); 
+        System.out.println(kata.equalsIgnoreCase(kata3)); 
+        
+    }
+    
+}

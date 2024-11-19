1ae4e492ed8c248d2cf82af8baa512b2b0f7476f
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-19 20:32:02 +0800

    hari 79
    String Method : toString()

---
+++ Day79.java
@@ -0,0 +1,21 @@
+package coding100day;
+
+public class Day_79 {
+    
+    public static void main(String[] args) {
+        
+        int a = 10;
+        boolean b = true;
+        char c = 'H';
+        
+        String a1 = Integer.toString(a);
+        String b1 = Boolean.toString(b);
+        String c1 = Character.toString(c);
+        
+        System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(c1);
+        
+    }
+    
+}

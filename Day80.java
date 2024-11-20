30eb595f78d7584490a2dfd9631818c7af6924b7
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-20 22:20:17 +0800

    hari 80
    String Method : trim()

---
+++ Day80.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+public class Day_80 {
+    
+    public static void main(String[] args) {
+        
+        String kamis = "     besok kuliah";
+        System.out.println("sebelum di trim : "+kamis);
+        
+        String b = kamis.trim();
+        System.out.println("setelah di trim : "+b);
+        
+    }
+    
+}

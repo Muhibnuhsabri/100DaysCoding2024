121b0fbf59bd78be46c0181f9a447f0e73e718a2
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-21 21:50:58 +0800

    hari 81
    String Method : valueOf()

---
+++ Day81.java
@@ -0,0 +1,16 @@
+package coding100day;
+
+public class Day_81 {
+    
+    public static void main(String[] args) {
+        
+        int a = 5;
+        String angka = String.valueOf(a);
+        System.out.println(angka);
+   
+        boolean kuliahBesok = true;
+        String kuliah = String.valueOf(kuliahBesok);
+        System.out.println(kuliah);
+        
+    }
+}

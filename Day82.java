3f095669b4c3e7c8327f63afe6d5474733358d6a
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-22 22:45:38 +0800

    hari 82
    Method ceil(), floor() dan round()

---
+++ Day82.java
@@ -0,0 +1,16 @@
+package coding100day;
+
+public class Day_82 {
+    
+    public static void main(String[] args) {
+        
+        double a = 16.1; 
+        double b = 30.9;
+        double c = 53.4;
+
+        System.out.println(a + " dijadikan ceil = " + Math.ceil(a));
+        System.out.println(b + " dijadikan floor = " + Math.floor(b));
+        System.out.println(c + " dijadikan round = " + Math.round(c));
+           
+    }
+}

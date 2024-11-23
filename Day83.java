1bde93833ee787d9da43fabfd95407625b96609f
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-23 23:39:12 +0800

    hari 83
    Membuat Objek Array satu dimensi

---
+++ Day83.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+public class Day_83 {
+    
+    public static void main(String[] args) {
+        
+        int[] angka = {10, 20, 30}; 
+
+        System.out.println(angka[0]); 
+        System.out.println(angka[1]); 
+        System.out.println(angka[2]);
+        
+    }
+    
+}

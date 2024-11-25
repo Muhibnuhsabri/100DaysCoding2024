e7f86cf4c12d3274b8c3b4b2d645444f1d5b8ae2
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-25 22:08:15 +0800

    hari 85
    Mengisi elemen ke Array satu dimensi pada saat di inisialisasi

---
+++ Day85.java
@@ -0,0 +1,12 @@
+package coding100day;
+import java.util.Arrays;
+public class Day_85 {
+    
+    public static void main(String[] args) {
+        
+        int[] a = {1,2,3,4,5};
+        
+        System.out.println(Arrays.toString(a));
+    }
+    
+}

0e66ac2a0aaf7dc0e15a44b74b618a2bad97b07a
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-26 22:55:00 +0800

    hari 86
    Mencetak elemen pada Array satu dimensi

---
+++ Day86.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+import java.util.Arrays;
+
+public class Day_86 {
+    
+    public static void main(String[] args) {
+        
+        int[] nomor = {10,20,30,40,50};
+        
+        System.out.println(Arrays.toString(nomor));
+        
+    }
+    
+}

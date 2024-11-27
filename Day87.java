9e8cd4523604770c07296e107f11b3388df50a92
Muh Ibnuh sabri <muhibnuhsabri@gmail.com>
2024-11-27 23:08:38 +0800

    hari 87
    Mengubah Nilai Array satu dimensi pada index Tertentu

---
+++ Day87.java
@@ -0,0 +1,15 @@
+package coding100day;
+
+import java.util.Arrays;
+
+public class Day_87 {
+
+    public static void main(String[] args) {
+
+        int[] angka = {10, 20, 30, 40, 50};
+        angka[2] = 20;
+
+        System.out.println(Arrays.toString(angka));
+    }
+
+}
